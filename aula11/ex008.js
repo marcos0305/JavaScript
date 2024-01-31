18679d2daec590df100c27385f947523f7bb5f3a
marcos0305 <carvalho.oliveira0305@gmail.com>
2024-01-30 21:41:43 -0300

    AULA 11 / Condições (Parte 1)

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 59
+console.log(`A velocidade do seu carro é de ${vel}km/h`)
+if(vel > 60){ //Condição simples.
+    console.log(`Voce ultrapassou a velocidade permitida.MULTADO!`)
+}
+console.log(`Dirija sempre usando cinto de segurança`)
---
+++ aula11/ex009.js
@@ -0,0 +1,6 @@
+var pais = 'BR'
+if(pais == 'BR'){
+    console.log('Você é Brasileiro')
+}else{
+    console.log('Você é estrangeiro')
+}
